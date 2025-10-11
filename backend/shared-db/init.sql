615799d5f8466f90e519dbd248e0e7094960f462
Ian Rayburn <ianrayburn9@gmail.com>
2025-10-11 19:03:25 -0400

    refactor: change sql event table to use camelCase instead of snake_case

--- backend/shared-db/init.sql
+++ backend/shared-db/init.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS events (
-    event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    eventId INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT NOT NULL,
     description TEXT,
-    start_time TEXT NOT NULL,
-    end_time TEXT NOT NULL,
+    startTime TEXT NOT NULL,
+    endTime TEXT NOT NULL,
     venue TEXT NOT NULL,
     capacity INTEGER NOT NULL CHECK (capacity >= 0),
-    available_tickets INTEGER NOT NULL CHECK (capacity >= 0 
-    AND available_tickets <= capacity)
+    availableTickets INTEGER NOT NULL CHECK (capacity >= 0 
+    AND availableTickets <= capacity)
 );
